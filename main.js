977133a1591ad57c95f065b9a1718bbb79156cc1
defskela <12832512749@leksFed>
2023-10-19 12:24:33 +0300

    Лабораторная работа по JavaScript, оптимизация кода

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-const button_myself = document.querySelector(".button_myself");
-const button_labs = document.querySelector(".button_labs");
+const button_myself = document.querySelector("#button_myself");
+const button_labs = document.querySelector("#button_labs");
 const labs = document.querySelector(".labs");
-const portfolio = document.querySelector(".portfolio");
 const aboutMe = document.querySelector(".aboutMe");
 
-function disabled_myself_information() {
-  if (aboutMe.style.display == "block") {
-    aboutMe.style.display = "none";
+function disabled_information(elem1, elem2) {
+  if (elem1.style.display == "block") {
+    elem1.style.display = "none";
   } else {
-    aboutMe.style.display = "block";
+    elem1.style.display = "block";
   }
-  if (labs.style.display == "block") {
-    labs.style.display = "none";
+  if (elem2.style.display == "block") {
+    elem2.style.display = "none";
   }
 }
 
-function disabled_labs_information() {
-  if (labs.style.display == "block") {
-    labs.style.display = "none";
-  } else {
-    labs.style.display = "block";
-  }
-  if (aboutMe.style.display == "block") {
-    aboutMe.style.display = "none";
-  }
-}
-
-button_labs.addEventListener("click", disabled_labs_information);
-button_myself.addEventListener("click", disabled_myself_information);
+button_labs.addEventListener("click", function () {
+  disabled_information(labs, aboutMe);
+});
+button_myself.addEventListener("click", function () {
+  disabled_information(aboutMe, labs);
+});
